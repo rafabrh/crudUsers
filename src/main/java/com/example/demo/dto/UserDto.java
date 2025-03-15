101ae1c62fb04fd61c3ceaa35905ff4c0706890a
Gabriel Milano <milano.developer@gmail.com>
2025-03-15 16:27:35 -0300

    Trocando Dto de classe para record

--- src/main/java/com/example/demo/dto/UserDto.java
+++ src/main/java/com/example/demo/dto/UserDto.java
@@ -1,16 +1,5 @@
 package com.example.demo.dto;
 
-public class UserDto {
-    private String name;
+public record UserDto(String name) {
 
-    public UserDto(String name) {
-        this.name = name;
-    }
-
-    public String getName() {
-        return name;
-    }
-    public void setName(String name) {
-        this.name = name;
-    }
 }
