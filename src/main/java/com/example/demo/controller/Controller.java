f1a6139a54824805f49ed443e16b0c66d01c77c7
Gabriel Milano <milano.developer@gmail.com>
2025-03-15 16:29:01 -0300

    Adaptando exception

--- src/main/java/com/example/demo/controller/Controller.java
+++ src/main/java/com/example/demo/controller/Controller.java
@@ -3,15 +3,16 @@ package com.example.demo.controller;
 
 import com.example.demo.dto.UserDto;
 import com.example.demo.entity.User;
+import com.example.demo.exception.IdDontExistException;
 import com.example.demo.repository.UserRepository;
 import com.example.demo.service.UserServices;
 import jakarta.transaction.Transactional;
-//import lombok.var;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import com.example.demo.exception.ResourceNotFoundException;
+
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -27,25 +28,15 @@ public class Controller {
         this.userRepository = userRepository;
     }
 
-
     @PostMapping
     public ResponseEntity<User> create(@RequestBody UserDto userDto) {
         User user = userServices.createUser(userDto);
         return new ResponseEntity<>(user, HttpStatus.CREATED);
     }
 
-    @GetMapping("/health")
-    public ResponseEntity<String> health() {
-        return new ResponseEntity<>("UP", HttpStatus.OK);
-
-
-    }
-
     @GetMapping("/listar")
     public ResponseEntity<List<UserDto>> listar() {
-
         List<User> users = userRepository.findAll();
-
         List<UserDto> userDtos = users.stream().map(user -> new UserDto(user.getName())).collect(Collectors.toList());
 
         return new ResponseEntity<>(userDtos, HttpStatus.OK);
@@ -53,31 +44,19 @@ public class Controller {
 
     @PutMapping("/atualizar/{id}")
     @Transactional
-    public ResponseEntity<User> update(@PathVariable Long id, @RequestBody @Validated UserDto userDto) {
-        // encontrar o usuário pelo ID
-        User user = userRepository.findById(id)
-                .orElseThrow(() -> new ResourceNotFoundException("User with ID" + id + "not found"));
-
-        // atualizar os dados do usuário
-        user.setName(userDto.getName());
-
-        // salva usuário atualizado
-        User updatedUser = userRepository.save(user);
-        return new ResponseEntity<>(updatedUser, HttpStatus.OK);
+    public User update(@PathVariable Long id, @RequestBody @Validated UserDto userDto) {
+        User user = userRepository.findById(id).orElseThrow(() -> new IdDontExistException("User with ID" + id + "not found"));
+        user.setName(userDto.name());
+        return user;
     }
 
     @DeleteMapping("/deletar/{id}")
     @Transactional
     public ResponseEntity<User> delete(@PathVariable Long id) {
-        // encontra o usuário pelo ID
-        User user = userRepository.findById(id)
-                .orElseThrow(() -> new ResourceNotFoundException("User with ID" + id + "not found"));
-
-        // deleta o usuário
+        User user = userRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("User with ID" + id + " not found"));
         userRepository.delete(user);
 
         return new ResponseEntity<>(user, HttpStatus.OK);
-
     }
 
 }
